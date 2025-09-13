81812df340db94ec23232e4038b88ee064d06e53
izaacpinheiro <izaacpinheiro.123@gmail.com>
2025-09-13 15:51:33 -0300

    build: adiciona projeto inicial

---
+++ src/main/java/com/ifceeventos/ifce_eventos/IfceEventosApplication.java
@@ -0,0 +1,13 @@
+package com.ifceeventos.ifce_eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfceEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfceEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifceeventos/ifce_eventos/IfceEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifceeventos.ifce_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfceEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
