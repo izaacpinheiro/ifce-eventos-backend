b20a99cd31e0cbee9e48a6a47432dd0e12fb1c3f
izaacpinheiro <izaacpinheiro.123@gmail.com>
2026-01-14 21:43:14 -0300

    feat: add web cors config

---
+++ src/main/java/com/ifceeventos/ifce_eventos/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.ifceeventos.ifce_eventos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // config do cors para a integração com o frontend
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
